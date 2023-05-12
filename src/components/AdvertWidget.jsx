bdd01475a582019ec985a2b4d7f4c68f6eb8fa46
Ikenna <iykee0333@gmail.com>
2023-05-12 17:58:35 +0100

    advert widget

---
+++ src/components/AdvertWidget.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Link, Typography, useTheme } from "@mui/material";
+import WidgetWrapper from "./WidgetWrapper";
+import FlexBetween from "./FlexBetween";
+
+const AdvertWidget = () => {
+	const { palette } = useTheme();
+	const dark = palette.neutral.dark;
+	const main = palette.neutral.main;
+	const medium = palette.neutral.medium;
+
+	return (
+		<WidgetWrapper>
+			<FlexBetween>
+				<Typography color={dark} variant="h5" fontWeight="500">
+					Sponsored
+				</Typography>
+				<Typography color={medium}>Create Ad</Typography>
+			</FlexBetween>
+			<img
+				width="100%"
+				height="auto"
+				alt="advert"
+				src="http://localhost:3001/assets/cosmetics.jpg"
+				style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
+			/>
+			<FlexBetween>
+				<Typography color={main}>Kimett Cosmetics</Typography>
+				<Link href="https://kimett.netlify.app" target="_blank" color={medium}>
+					kimett.com
+				</Link>
+			</FlexBetween>
+			<Typography color={medium} m="0.5rem 0">
+				Your pathway to stunning beauty and amazing skin...
+			</Typography>
+		</WidgetWrapper>
+	);
+};
+
+export default AdvertWidget;
